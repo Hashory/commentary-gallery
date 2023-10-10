b70a45f9a310e4fc60296c90828da716e151ed62
Hashory <89839285+Hashory@users.noreply.github.com>
2023-10-10 10:50:40 +0900

    WIP

---
+++ src/lib/ImageView.svelte
@@ -0,0 +1,14 @@
+<script>
+	
+</script>
+
+<div>
+	<div class="menu">
+		<!-- ポインター 範囲指定ズーム | カーソル色のまわり カーソル残像 -->
+	</div>
+
+</div>
+
+<style>
+
+</style>
---
+++ src/lib/List.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Masonry from 'svelte-bricks'
+
+  let nItems = 30
+  $: items = [...Array(nItems).keys()]
+
+  let [minColWidth, maxColWidth, gap] = [200, 800, 20]
+  let width: number | undefined = undefined;
+	let height: number | undefined = undefined;
+</script>
+
+<Masonry
+  {items}
+  {minColWidth}
+  {maxColWidth}
+  {gap}
+  let:item
+  bind:masonryWidth={width}
+  bind:masonryHeight={height}
+>
+  <img src="" alt="aaa">
+</Masonry>
